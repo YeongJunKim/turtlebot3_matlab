14cf7b66663f508fcdb13b64053fbe295312e159
colson <dud3722000@naver.com>
2019-10-10 16:24:16 +0900

    .

--- timer_.m
+++ timer_.m
@@ -1,15 +1,12 @@
-function timer_(obj, event, robots)
+function timer_(obj, event, robots, filters)
     persistent step;
     persistent firstRun;
-        
     persistent var;
-    persistent f h jac_f jac_h;
     
     if isempty(firstRun)
        step = 0; 
               
        %% init task
-       % additional subscriber
        for i = 1:size(robots)
            var.sub_odom_data(i).data = robots(i).sub_odom_data;
        end
@@ -19,20 +16,16 @@ function timer_(obj, event, robots)
        for i = 2:size(robots)
            var.sub_scan_data(i).data = robots(i).sub_scan_data;
        end
+       
+       % additional subscriber
        var.sub_measurement_data = [];
        var.sub_measurement_past = [];
        var.sub_measurement_now = [];
        var.sub_measurement_raw = [];
-       var.data = [];
-       
-       turtlebot3_addSubscrier(robots(1),'/tb3_0/read', @measurement_data);
+       turtlebot3_addSubscrier(robots(1),'/read', @measurement_data);
        pause(2);
        
-       %% init filter
-       
-       
-       
-       
+
        firstRun = 1;
     end
     
@@ -47,9 +40,14 @@ function timer_(obj, event, robots)
     for i = 2:size(robots,2)
        var.sub_scan_data(i).data = robots(i).sub_scan_data;
     end
-    
     var.sub_measurement_data = robots(1).sub_data(1).data.Data;
     var.sub_measurement_raw = extractBetween(var.sub_measurement_data,"a 1 "," b");
+    
+    if size(var.sub_measurement_raw, 1) == 0
+        var.sub_measurement_raw = cell(1,1);
+        var.sub_measurement_raw(1) = {'0 0 0 0'};
+    end
+    
     var.sub_measurement_now = cell2mat(var.sub_measurement_raw(1));
     if ischar(var.sub_measurement_now)
         var.sub_measurement_now = str2num(var.sub_measurement_now);
@@ -62,7 +60,7 @@ function timer_(obj, event, robots)
        var.sub_measurement_now = [0 0 0 0];
     end
     var.sub_measurement_now = (0.001)*var.sub_measurement_now;
-    disp(var.sub_measurement_now);    
+%     disp(var.sub_measurement_now);    
     
     for i = 1:size(robots,2)
         var.data(i).odom = var.sub_odom_data(i).data.Pose.Pose;
@@ -70,22 +68,27 @@ function timer_(obj, event, robots)
         var.data(i).imu = quat2eul(imu, 'ZYX');
         yaw = var.data(i).imu(3); % TODO calibration
         var.data(i).yaw = wrapToPi(yaw);
-        disp(i)
-        disp(var.data(i))
+%         disp(var.data(i))
     end
     toc;
     tic;
     %% localization
-    
+        x_hat = filtering_run(filters(1), [0.02 0]', [1, 1, 1, 1, 1]', 0);
+        disp(x_hat);
     toc;
     
     step = step + 1;
 end
 
-function calc_measurement_data(data)
 
 
-end
+
+
+
+
+
+
+
 function measurement_data(src, msg, obj)
 %      disp('measurement data');
 %        disp(var.sub_measurement_data.data);
--- turtlebot3_localization/localization.m
+++ turtlebot3_localization/localization.m
@@ -1,15 +1,25 @@
 classdef localization < handle
-   properties 
-       %% variable area
+   properties
+       %% init
+       P, Q, R, firstRun;
        f,h,jac_f,jac_h;
+       %% variable area
+       u;
+       z;       
+       x_hat;
+       
+       alpha;
    end
    methods
        %% function area
-       function localization_init(f_,h_,jac_f_,jac_h_)
-          f = f_;
-          h = h_;
-          jac_f = jac_f_;
-          jac_h = jac_h_;
+       function localization_init(obj, f_, h_, jac_f_, jac_h_, P_, Q_, R_)
+          obj.f = f_;
+          obj.h = h_;
+          obj.jac_f = jac_f_;
+          obj.jac_h = jac_h_;
+          obj.P = P_;
+          obj.Q = Q_;
+          obj.R = R_;
        end
        
    end

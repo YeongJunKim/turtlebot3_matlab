669eaf00650d046a4d5599cccca8987fa3349d60
colson <dud3722000@naver.com>
2019-10-21 15:42:48 +0900

    .

--- distributed_localization.m
+++ distributed_localization.m
@@ -38,42 +38,44 @@ end
 %% filter init
 %% init filter
 % Position information of Anchors
-distance = 0.6 * 10;
-x1 = 0; y1 = 0;
-x2 = 0; y2 = distance;
-x3 = distance; y3 = distance;
-x4 = distance; y4 = 0;
-syms x y theta
-syms u1 u2
-f1(x,y,theta,u1,u2) = x + u1 * cos(theta + 1/2 * u2);
-f2(x,y,theta,u1,u2) = y + u1 * sin(theta + 1/2 * u2);
-f3(x,y,theta,u1,u2) = theta + u2;
-f = [f1 f2 f3]';
-clear  f1 f2 f3
-jac_f = jacobian(f, [x, y, theta]);
-h1(x,y,theta,u1,u2) = sqrt((x-x1)^2 + (y-y1)^2) - sqrt((x-x2)^2 + (y-y2)^2);
-h2(x,y,theta,u1,u2) = sqrt((x-x1)^2 + (y-y1)^2) - sqrt((x-x3)^2 + (y-y3)^2);
-h3(x,y,theta,u1,u2) = sqrt((x-x1)^2 + (y-y1)^2) - sqrt((x-x4)^2 + (y-y4)^2);
-h4(x,y,theta,u1,u2) = theta;
-h= [h1 h2 h3 h4]';
-clear h1 h2 h3 h4
-jac_h = jacobian(h,[x y theta]);
-f = matlabFunction(f);
-jac_f = matlabFunction(jac_f);
-h = matlabFunction(h);
-jac_h = matlabFunction(jac_h);
-method = "PEFFME";       
-filters(1) = filtering;
-filtering_init(filters(1), method, f, h, jac_f, jac_h);
-filtering_set_h_size(filters(1), 5);
-filters(1).x_hat_pre = [3 3 0]';
-filters(2) = filtering;
-filters(3) = filtering;
+
+
+% distance = 0.6 * 10;
+% x1 = 0; y1 = 0;
+% x2 = 0; y2 = distance;
+% x3 = distance; y3 = distance;
+% x4 = distance; y4 = 0;
+% syms x y theta
+% syms u1 u2
+% f1(x,y,theta,u1,u2) = x + u1 * cos(theta + 1/2 * u2);
+% f2(x,y,theta,u1,u2) = y + u1 * sin(theta + 1/2 * u2);
+% f3(x,y,theta,u1,u2) = theta + u2;
+% f = [f1 f2 f3]';
+% clear  f1 f2 f3
+% jac_f = jacobian(f, [x, y, theta]);
+% h1(x,y,theta,u1,u2) = sqrt((x-x1)^2 + (y-y1)^2) - sqrt((x-x2)^2 + (y-y2)^2);
+% h2(x,y,theta,u1,u2) = sqrt((x-x1)^2 + (y-y1)^2) - sqrt((x-x3)^2 + (y-y3)^2);
+% h3(x,y,theta,u1,u2) = sqrt((x-x1)^2 + (y-y1)^2) - sqrt((x-x4)^2 + (y-y4)^2);
+% h4(x,y,theta,u1,u2) = theta;
+% h= [h1 h2 h3 h4]';
+% clear h1 h2 h3 h4
+% jac_h = jacobian(h,[x y theta]);
+% f = matlabFunction(f);
+% jac_f = matlabFunction(jac_f);
+% h = matlabFunction(h);
+% jac_h = matlabFunction(jac_h);
+% method = "PEFFME";       
+% filters(1) = filtering;
+% filtering_init(filters(1), method, f, h, jac_f, jac_h);
+% filtering_set_h_size(filters(1), 5);
+% filters(1).x_hat_pre = [3 3 0]';
+% filters(2) = filtering;
+% filters(3) = filtering;
 
 
 
 
 
-tm = timer('BusyMode', 'drop', 'ExecutionMode', 'fixedRate', 'Period', time_interval, 'TimerFcn', {@timer_, robots, filters});
+tm = timer('BusyMode', 'drop', 'ExecutionMode', 'fixedRate', 'Period', time_interval, 'TimerFcn', {@timer_, robots});
 start(tm);
 
--- timer_.m
+++ timer_.m
@@ -1,12 +1,21 @@
-function timer_(obj, event, robots, filters)
+function timer_(obj, event, robots)
     persistent step;
     persistent firstRun;
-    persistent var;
+    global var;
     
     if isempty(firstRun)
        step = 0; 
               
        %% init task
+       var.sub_odom_data = [];
+       var.sub_imu_data = [];
+       var.sub_scan_data = [];
+       var.sub_measurement_data = [];
+       var.sub_measurement_past = [];
+       var.sub_measurement_now = [];
+       var.sub_measurement_raw = [];
+       var.data = [];
+       
        for i = 1:size(robots)
            var.sub_odom_data(i).data = robots(i).sub_odom_data;
        end
@@ -18,31 +27,49 @@ function timer_(obj, event, robots, filters)
        end
        
        % additional subscriber
-       var.sub_measurement_data = [];
-       var.sub_measurement_past = [];
-       var.sub_measurement_now = [];
-       var.sub_measurement_raw = [];
+       pause(1);
        turtlebot3_addSubscrier(robots(1),'/read', @measurement_data);
-       pause(2);
+       pause(1);
        
 
        firstRun = 1;
+    end    
+    step = step + 1;
+    disp(step)
+    if step > 10
+        %% get data
+        tic;
+        for i = 1:size(robots,2)
+           var.sub_odom_data(i).data = robots(i).sub_odom_data;
+        end
+        for i = 1:size(robots,2)
+           var.sub_imu_data(i).data = robots(i).sub_imu_data;
+        end
+        for i = 2:size(robots,2)
+           var.sub_scan_data(i).data = robots(i).sub_scan_data;
+        end
+        toc;
+        %% localization
+
+        toc;
     end
-    
-    %% get data
-    tic;
-    for i = 1:size(robots,2)
-       var.sub_odom_data(i).data = robots(i).sub_odom_data;
-    end
-    for i = 1:size(robots,2)
-       var.sub_imu_data(i).data = robots(i).sub_imu_data;
-    end
-    for i = 2:size(robots,2)
-       var.sub_scan_data(i).data = robots(i).sub_scan_data;
-    end
-    var.sub_measurement_data = robots(1).sub_data(1).data.Data;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+function measurement_data(src, msg, obj)
+    obj.sub_data(1).data = msg;
+    var.sub_measurement_data = msg.Data;
     var.sub_measurement_raw = extractBetween(var.sub_measurement_data,"a 1 "," b");
-    
     if size(var.sub_measurement_raw, 1) == 0
         var.sub_measurement_raw = cell(1,1);
         var.sub_measurement_raw(1) = {'0 0 0 0'};
@@ -60,61 +87,5 @@ function timer_(obj, event, robots, filters)
        var.sub_measurement_now = [0 0 0 0];
     end
     var.sub_measurement_now = (0.001)*var.sub_measurement_now;
-%     disp(var.sub_measurement_now);    
-    
-    for i = 1:size(robots,2)
-        var.data(i).odom = var.sub_odom_data(i).data.Pose.Pose;
-        imu = [var.data(i).odom.Orientation.X, var.data(i).odom.Orientation.Y, var.data(i).odom.Orientation.Z, var.data(i).odom.Orientation.W];
-        var.data(i).imu = quat2eul(imu, 'ZYX');
-        yaw = var.data(i).imu(3); % TODO calibration
-        var.data(i).yaw = wrapToPi(yaw);
-%         disp(var.data(i))
-    end
-    toc;
-    tic;
-    %% localization
-        x_hat = filtering_run(filters(1), [0.02 0]', [1, 1, 1, 1, 1]', 0);
-        disp(x_hat);
-    toc;
-    
-    step = step + 1;
-end
-
-
-
-
-
-
-
-
-
-
-function measurement_data(src, msg, obj)
-%      disp('measurement data');
-%        disp(var.sub_measurement_data.data);
-    obj.sub_data(1).data = msg;
-%     disp(obj.sub_data(1).data.Data);
-end
-function measurement_data1(src, msg, arg, var)
-%      disp('measurement data');
-       var.sub_measurement_data.data = msg;
-       disp(var.sub_measurement_data.data);
-       
-       raw_data = msg.Data;
-       raw_data = extractBetween(raw_data,"a 1"," b");
-       data = cell2mat(raw_data);
-       
-       if ischar(data)
-          data = str2num(data);
-          var.sub_measurement_data = data;
-          var.sub_measurement_pre_data = data;
-       else
-          var.sub_measurement_data = var.sub_measurement_pre_data;
-       end
-       
-       if size(var.sub_measurement_data) < 4
-          var.measurement_data = [0 0 0 0]; 
-       end
-       
-       var.measurement_data = (1/1000)*var.measurement_data; 
+%     disp(var.sub_measurement_now);  
 end

bb46a7827052de9283fb3fb3821d27be0aa42962
colson <dud3722000@naver.com>
2020-11-03 22:52:50 +0900

    .

--- turtlebot3/turtlebot3_default.m
+++ turtlebot3/turtlebot3_default.m
@@ -58,7 +58,7 @@ classdef turtlebot3_default < handle
     end
     methods
         %% function area
-        function obj = turtlebot3_default(namespace, iscallbcak)
+        function obj = turtlebot3_default(namespace, iscallbcak, isknown)
             % arguments
             % namespace  : each robot's ROS namespace ex) (ROS_NAMESPACE=colsonbot roslaunch ~)
             disp(namespace)
@@ -66,31 +66,39 @@ classdef turtlebot3_default < handle
             obj.namespace = namespace;
             obj.cmd_vel = zeros(2,1);
             if iscallbcak == 1
-            obj.sub_imu = rossubscriber(strcat(namespace,'/imu'), {@sub_imu_callback, obj});
-            obj.sub_scan = rossubscriber(strcat(namespace,'/scan'), {@sub_scan_callback, obj});
-            obj.sub_odom = rossubscriber(strcat(namespace,'/odom'), {@sub_odom_callback, obj});
-            obj.sub_ahrsv1 = rossubscriber(strcat(namespace,'/mw_ahrsv1/imu'), {@sub_ahrsv1_callback, obj});
+                obj.sub_imu = rossubscriber(strcat(namespace,'/imu'), {@sub_imu_callback, obj});
+                obj.sub_scan = rossubscriber(strcat(namespace,'/scan'), {@sub_scan_callback, obj});
+                obj.sub_odom = rossubscriber(strcat(namespace,'/odom'), {@sub_odom_callback, obj});
+                obj.sub_ahrsv1 = rossubscriber(strcat(namespace,'/mw_ahrsv1/imu'), {@sub_ahrsv1_callback, obj});
+                obj.sub_input  = rossubscriber(strcat(namespace,'/cmd_vel'), {@sub_cmd_vel_callbcak, obj});
             else
-            obj.sub_imu = rossubscriber(strcat(namespace,'/imu'));
-            obj.sub_scan = rossubscriber(strcat(namespace,'/scan'));
-            obj.sub_odom = rossubscriber(strcat(namespace,'/odom'));
-            obj.sub_ahrsv1 = rossubscriber(strcat(namespace,'/mw_ahrsv1/imu'));
-            obj.sub_input  = rossubscriber(strcat(namespace,'/cmd_vel'), {@sub_cmd_vel_callbcak, obj});
+                obj.sub_imu = rossubscriber(strcat(namespace,'/imu'));
+                if isknown ~= 1
+                    obj.sub_scan = rossubscriber(strcat(namespace,'/scan'));
+                end
+                obj.sub_odom = rossubscriber(strcat(namespace,'/odom'));
+                obj.sub_ahrsv1 = rossubscriber(strcat(namespace,'/mw_ahrsv1/imu'));
+                obj.sub_input  = rossubscriber(strcat(namespace,'/cmd_vel'), {@sub_cmd_vel_callbcak, obj});
             end
             
             % publisher
             [obj.pub_control, obj.pub_control_msg] = rospublisher(strcat(namespace,'/cmd_vel'), 'geometry_msgs/Twist');
         end
         
-        function obj = turtlebot3_addSubscriver(obj, topic, callback)
+        function obj = turtlebot3_addSubscriver(obj, topic, callback, flag)
             disp("add subscriber");
             
-            if(callback ~= 0)
+            if flag == 1
                 obj.sub_type(obj.addcount).data = rossubscriber(topic, {callback, obj});
             else
                 obj.sub_type(obj.addcount).data = rossubscriber(topic);
             end
-            
+            %             if(callback ~= 0)
+            %                 obj.sub_type(obj.addcount).data = rossubscriber(topic, {callback, obj});
+            %             else
+            %                 obj.sub_type(obj.addcount).data = rossubscriber(topic);
+            %             end
+            %
             obj.sub_added(obj.addcount).data = obj.sub_type;
             obj.addcount = obj.addcount + 1;
         end
@@ -113,7 +121,7 @@ end
 %% callback
 
 function sub_cmd_vel_callbcak(src, msg, obj)
-    sub_cmd_vel = msg;
+sub_cmd_vel = msg;
 v_l = sub_cmd_vel.Linear.X;
 v_a = sub_cmd_vel.Angular.Z;
 temp = [v_l v_a]';
@@ -123,31 +131,15 @@ end
 
 function sub_imu_callback(src, msg, obj)
 obj.sub_imu_data = msg;
-% obj.imu_theta = obj.sub_imu_data.Orientation.W;
-% Need to convert quat to eul
-% obj.sub_imu_data
-% obj.sub_imu_data.LinearVelocity
 imu = obj.sub_imu_data.Orientation;
 temp = [imu.X imu.Y imu.Z imu.W];
 temp = quat2eul(temp, 'ZYX');
 obj.imu_theta = temp(3);
-if obj.run_time_info == 1
-    obj.imu_theta(end+1) = toc;
-end
 end
 function sub_scan_callback(src, msg, obj)
 obj.sub_scan_data = msg;
-% if(obj.lidar_seq ~= 0)
-    obj.lidar_seq = obj.lidar_seq + 1;
-    obj.lidar_data(1:360) = obj.sub_scan_data.Ranges(:);
-%     x = obj.lidar_data(:)' .* cos(deg2rad(1+obj.lidar_rotation_angle:obj.lidar_accuracy+obj.lidar_rotation_angle));
-%     y = obj.lidar_data(:)' .* sin(deg2rad(1+obj.lidar_rotation_angle:obj.lidar_accuracy+obj.lidar_rotation_angle));
-%     obj.lidar_xy(1,:) = x(:); obj.lidar_xy(2,:) = y(:);
-%     if obj.run_time_info == 1
-        obj.lidar_data(361) = toc;
-%     end
-%     disp(obj.namespace);
-% end
+obj.lidar_seq = obj.lidar_seq + 1;
+obj.lidar_data = obj.sub_scan_data.Ranges(:);
 end
 function sub_odom_callback(src, msg, obj)
 obj.sub_odom_data = msg;
@@ -161,17 +153,9 @@ obj.v_a = obj.sub_odom_data.Twist.Twist.Angular.Z;
 temp = [odom.Orientation.X, odom.Orientation.Y, odom.Orientation.Z, odom.Orientation.W];
 temp = quat2eul(temp, 'ZYX');
 obj.theta =  temp(3);
-if obj.run_time_info == 1
-    obj.v_l(end+1) = toc;
-    obj.v_a(end+1) = toc;
-    obj.theta(end+1) = toc;
-end
 end
 function sub_ahrsv1_callback(src, msg, obj)
 obj.sub_ahrsv1_data = msg;
 ahrsv1 = obj.sub_ahrsv1_data.Orientation;
 obj.ahrsv1 = ahrsv1.Z;
-if obj.run_time_info == 1
-    obj.ahrsv1(end+1) = toc;
-end
 end
